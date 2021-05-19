fb3f56ea2cf37075463eca644465c3bc00e19e27
Xue Wenchao <65531442+Axzed@users.noreply.github.com>
2021-05-19 12:49:02 +0800

    暴力无脑求解

--- OJ-Practice/LeetCode/1.两数之和/code1.cpp
+++ OJ-Practice/LeetCode/1.两数之和/code1.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> ans;
+        //两层循环完事
         for(int i = 0; i < nums.size(); i ++)
             for(int j = i + 1; j < nums.size(); j ++)
             {
