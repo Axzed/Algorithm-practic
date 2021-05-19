8991d313961193b2bb2155010b809bd96d5e7ccf
Xue Wenchao <65531442+Axzed@users.noreply.github.com>
2021-05-19 12:47:17 +0800

    暴力无脑遍历求解

---
+++ OJ-Practice/LeetCode/1.两数之和/code1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> ans;
+        for(int i = 0; i < nums.size(); i ++){
+            int t = target - nums[i];
+            if(ans.count(t)) return {ans[t], i};
+            ans[nums[i]] = i;
+        }
+        return {};
+    }
+};
