6741dd8518e109dc45222187617fb678019413e3
Xue Wenchao <65531442+Axzed@users.noreply.github.com>
2021-05-19 12:47:52 +0800

    Update code1.cpp

--- OJ-Practice/LeetCode/1.两数之和/code1.cpp
+++ OJ-Practice/LeetCode/1.两数之和/code1.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> ans;
-        for(int i = 0; i < nums.size(); i ++){
-            int t = target - nums[i];
-            if(ans.count(t)) return {ans[t], i};
-            ans[nums[i]] = i;
-        }
-        return {};
+        vector<int> ans;
+        for(int i = 0; i < nums.size(); i ++)
+            for(int j = i + 1; j < nums.size(); j ++)
+            {
+                if(nums[i] + nums[j] == target) 
+                {
+                    ans.push_back(i);
+                    ans.push_back(j);
+                }
+            }
+        return ans;
     }
 };
